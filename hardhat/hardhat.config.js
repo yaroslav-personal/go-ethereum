00fcb3a6a139166fb8ad55460bd9ad3a983b5ab7
Yaroslav Markovski <yorimark@abv.bg>
2023-11-01 09:26:34 +0200

    Update hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -3,4 +3,9 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    localhost: {
+      url: "http://localhost:8545",
+    },
+  }
 };
